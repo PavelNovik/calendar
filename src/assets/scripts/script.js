0de1369a0797f6adc2058a1ea2348e06f2f67579
PavelNovik <92117738+PavelNovik@users.noreply.github.com>
2023-07-10 15:31:01 +0300

    Add function live-server and tacking data from db.json

--- src/assets/scripts/script.js
+++ src/assets/scripts/script.js
@@ -3,10 +3,25 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-icons/font/bootstrap-icons.css'; // needs additional webpack config!
 
 import bootstrap5Plugin from '@fullcalendar/bootstrap5';
+import { dA } from '@fullcalendar/core/internal-common';
+import { preventDefault } from '@fullcalendar/core/internal';
 
 document.addEventListener('DOMContentLoaded', function () {
   const calendarEl = document.getElementById('calendar');
   const calendar = new FullCalendar.Calendar(calendarEl, {
+    eventSources: [
+      {
+        events: function (info, successCallback, failureCallback) {
+          fetch('http://localhost:8080/events')
+            .then((response) => response.json())
+            .then((result) => {
+              successCallback(result);
+            });
+        },
+        // color: 'rgb(25, 10, 226)', // an option!
+        // textColor: 'white', // an option!
+      },
+    ],
     plugins: [bootstrap5Plugin],
     themeSystem: 'bootstrap5',
     windowResizeDelay: 50,
@@ -18,7 +33,16 @@ document.addEventListener('DOMContentLoaded', function () {
     showNonCurrentDates: false,
     weekNumbers: true,
     selectable: true,
+
+    eventClick: function (data) {
+      console.log(data.event.title);
+      console.log(data.event.id);
+    },
+
     select: function (selectionInfo) {
+      $('#dialog').dialog('open');
+      $('#start').val(selectionInfo.startStr);
+      $('#end').val(selectionInfo.endStr);
       console.log(
         'selection is ',
         selectionInfo.startStr,
@@ -26,9 +50,33 @@ document.addEventListener('DOMContentLoaded', function () {
         selectionInfo.endStr
       );
     },
-    dateClick: function (info) {
-      console.log('clicked on ' + info.dateStr);
+    dateClick: function (date) {
+      // console.log(date);
+      const clickDate = date.dateStr;
+      console.log('clicked on ' + clickDate);
+      $('#dialog').dialog('open');
+      $('#start').val(clickDate);
     },
   });
   calendar.render();
+});
+$(function () {
+  $('#dialog').dialog({
+    autoOpen: false,
+    show: {
+      effect: 'fold',
+    },
+    hide: {
+      effect: 'fold',
+    },
+  });
+
+  $('.datepicker').datepicker({
+    dateFormat: 'yy-mm-dd',
+  });
+
+  $('form').submit(function (e) {
+    e.preventDefault();
+    console.log(e.target);
+  });
 });
